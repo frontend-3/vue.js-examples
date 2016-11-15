c81dfecfee56873d07bdb092de9a4d639537fa84
Elizabeth Manrique <ecmanrique@gmail.com>
2016-11-15 16:58:14 -0500

    adding example for directives

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-// import App from './App'
 /* eslint-disable no-new */
 
-// [EXAMPLE 1] Declarative render
+/* NOTE: Just comment and discoment if you want to run any specific example */
+
+/* [EXAMPLE 1] Declarative render */
 new Vue({
   el: '#app',
   data: {
@@ -10,7 +11,15 @@ new Vue({
   }
 })
 
-// [EXAMPLE] Components
+/* [EXAMPLE 2] Using directives */
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
+
+/* [EXAMPLE] Components */
 // new Vue({
 //   el: '#app',
 //   template: '<App/>',
