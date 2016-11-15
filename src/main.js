0d534300a0ad41ffe8c0c14c47ebd231ac11527e
Elizabeth Manrique <ecmanrique@gmail.com>
2016-11-15 16:55:23 -0500

    adding first example of declarative render

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
-import App from './App'
-
+// import App from './App'
 /* eslint-disable no-new */
+
+// [EXAMPLE 1] Declarative render
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  data: {
+    message: 'Hello Vue.js!'
+  }
 })
+
+// [EXAMPLE] Components
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
